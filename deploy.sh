4ed35681cd13306810f8b4a935091273f4826c49
Kunal <jainkunal3004@gmail.com>
2023-11-08 18:59:04 +0000

    New sqlx prepare script + deploy script

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+password=$(aws ecr get-login-password --region us-east-1)
+echo $password | docker login --username AWS --password-stdin 414942293597.dkr.ecr.us-east-1.amazonaws.com
+docker buildx build --platform linux/amd64 -t walnut-server-pipeline .
+docker tag walnut-server-pipeline:latest 414942293597.dkr.ecr.us-east-1.amazonaws.com/walnut-server-pipeline:latest
+docker push 414942293597.dkr.ecr.us-east-1.amazonaws.com/walnut-server-pipeline:latest
+aws ecs update-service --force-new-deployment --service WalnutServer-east-1 --cluster WalnutServer-east-1
