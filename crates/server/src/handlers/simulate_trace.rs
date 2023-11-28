9062ff55b4a25bf107aeb6e08edc8e055430b771
Kunal Jain <kunal@joinwido.com>
2023-11-28 18:39:19 +0530

    use SimulatedTransaction as response type

--- crates/server/src/handlers/simulate_trace.rs
+++ crates/server/src/handlers/simulate_trace.rs
@@ -1,6 +1,5 @@
 use crate::app_state::AppState;
 use crate::handlers::simulations::SimulationRes;
-use crate::types::TransactionExecutionInfo;
 use crate::utils::simulate::convert_to_hex;
 use crate::utils::simulate::create_fork_cached_state_at;
 use crate::utils::simulate::get_block_context;
@@ -10,11 +9,15 @@ use axum::{
     Json,
 };
 use blockifier::state::cached_state::CachedState;
+use blockifier::transaction::objects::TransactionExecutionInfo;
 use blockifier::transaction::transaction_execution::Transaction;
 use blockifier::transaction::transactions::ExecutableTransaction;
 use serde::Serialize;
 use sqlx::types::Uuid;
-use starknet::core::types::BlockId;
+use starknet::core::types::{
+    BlockId, CallType, EventContent, ExecuteInvocation, FeeEstimate, FieldElement,
+    FunctionInvocation, InvokeTransactionTrace, SimulatedTransaction, TransactionTrace,
+};
 use starknet_api::block::BlockNumber;
 use starknet_api::core::{ChainId, ContractAddress, Nonce, PatriciaKey};
 use starknet_api::hash::{StarkFelt, StarkHash};
@@ -28,7 +31,7 @@ use std::{str::FromStr, sync::Arc};
 
 #[derive(Serialize)]
 pub struct SimulateTraceResponse {
-    execution_info: TransactionExecutionInfo,
+    simulated_transaction: SimulatedTransaction,
     simulation: SimulationRes,
 }
 
@@ -103,9 +106,105 @@ pub async fn simulate_trace(
 
     match tx_info {
         Ok(tx_info) => Ok(Json(SimulateTraceResponse {
-            execution_info: tx_info.into(),
+            simulated_transaction: to_simulated_transaction(tx_info),
             simulation: sim,
         })),
         Err(_) => Err(StatusCode::EXPECTATION_FAILED),
+    }
+}
+
+fn to_simulated_transaction(tx: TransactionExecutionInfo) -> SimulatedTransaction {
+    let eci = tx.execute_call_info.unwrap();
+
+    SimulatedTransaction {
+        transaction_trace: TransactionTrace::Invoke(InvokeTransactionTrace {
+            execute_invocation: ExecuteInvocation::Success(to_function_invocation(&eci)),
+            // TODO: Implement this
+            validate_invocation: None,
+            fee_transfer_invocation: None,
+        }),
+        // TODO: Implement this
+        fee_estimation: FeeEstimate {
+            gas_consumed: 0,
+            gas_price: 0,
+            overall_fee: 0,
+        },
+    }
+}
+
+fn to_function_invocation(eci: &blockifier::execution::call_info::CallInfo) -> FunctionInvocation {
+    let calldata = eci
+        .call
+        .calldata
+        .0
+        .iter()
+        .map(|x| FieldElement::from_byte_slice_be(x.bytes()).unwrap())
+        .collect();
+
+    let events: Vec<EventContent> = eci
+        .execution
+        .events
+        .iter()
+        .map(|x| EventContent {
+            data: x
+                .event
+                .data
+                .0
+                .iter()
+                .map(|x| FieldElement::from(*x))
+                .collect(),
+            keys: x
+                .event
+                .keys
+                .iter()
+                .map(|x| FieldElement::from(x.0))
+                .collect(),
+        })
+        .collect();
+
+    let result = eci
+        .execution
+        .retdata
+        .0
+        .iter()
+        .map(|x| FieldElement::from(*x))
+        .collect();
+
+    let internal_calls: Vec<FunctionInvocation> = eci
+        .inner_calls
+        .iter()
+        .map(|x| to_function_invocation(x))
+        .collect();
+
+    FunctionInvocation {
+        calldata: calldata,
+        contract_address: FieldElement::from(*Into::<&StarkFelt>::into(
+            eci.call.storage_address.0.key(),
+        )),
+        call_type: match eci.call.call_type {
+            blockifier::execution::entry_point::CallType::Call => CallType::Call,
+            blockifier::execution::entry_point::CallType::Delegate => CallType::LibraryCall,
+        },
+        caller_address: FieldElement::from(*Into::<&StarkFelt>::into(
+            eci.call.caller_address.0.key(),
+        )),
+        class_hash: FieldElement::from(eci.call.class_hash.unwrap().0),
+        entry_point_type: match eci.call.entry_point_type {
+            starknet_api::deprecated_contract_class::EntryPointType::Constructor => {
+                starknet::core::types::EntryPointType::Constructor
+            }
+            starknet_api::deprecated_contract_class::EntryPointType::External => {
+                starknet::core::types::EntryPointType::External
+            }
+            starknet_api::deprecated_contract_class::EntryPointType::L1Handler => {
+                starknet::core::types::EntryPointType::L1Handler
+            }
+        },
+        events: events,
+        // TODO: Implement this
+        messages: vec![],
+        result: result,
+        entry_point_selector: FieldElement::from(eci.call.entry_point_selector.0),
+        calls: internal_calls,
     }
 }
