de7c6ea60bb5b123e73aaaf974da3f823440640a
Roman <barabanov1233@gmail.com>
2024-09-12 13:43:21 +0400

    Fix: Set scarb cache path for sozo

--- crates/verification/src/sozo.rs
+++ crates/verification/src/sozo.rs
@@ -1,10 +1,10 @@
 use anyhow::Result;
 use cairo_lang_starknet_classes::contract_class::ContractClass;
-use std::fs;
 use std::fs::File;
 use std::io::BufReader;
 use std::path::PathBuf;
 use std::process::{Command, Stdio};
+use std::{env, fs};
 use tracing::error;
 use walkdir::WalkDir;
 
@@ -14,18 +14,43 @@ pub const SUPPORTED_DOJO_ALPHA_VERSIONS: &[u8] = &[9, 11];
 
 fn run_sozo_build(tmp_dir: &PathBuf, sozo_path: &str) -> Result<()> {
     let absolute_path = fs::canonicalize(sozo_path)?;
-    let status = Command::new(absolute_path)
+
+    let scarb_cache_dir = env::current_dir()?.join(".cache/scarb");
+    let scarb_cache_dir_str = scarb_cache_dir.to_str().ok_or_else(|| {
+        error!("Error converting cache directory to string");
+        anyhow::anyhow!("Failed to convert cache dir to string")
+    })?;
+
+    let mut cmd = Command::new(absolute_path);
+
+    cmd.env("SCARB_CACHE", scarb_cache_dir_str)
         .arg("build")
         .current_dir(tmp_dir)
         .stdout(Stdio::inherit())
-        .stderr(Stdio::inherit())
-        .status()?;
+        .stderr(Stdio::inherit());
+
+    let status = cmd.status()?;
 
     if status.success() {
         Ok(())
     } else {
-        error!("`sozo` exited with error: {:?}", status);
-        Err(anyhow::anyhow!("`sozo` exited with error: {:?}", status))
+        let output = match cmd.output() {
+            Ok(output) => output,
+            Err(e) => {
+                let error_message = format!(
+                    "Failed to execute `sozo`, status: {:?}, failed to get output: {:?}",
+                    status, e
+                );
+                error!("{}", error_message);
+                return Err(anyhow::anyhow!(error_message));
+            }
+        };
+        let error_message = format!(
+            "`sozo` exited with error: {:?}, status: {:?}",
+            output, status
+        );
+        error!("{}", error_message);
+        Err(anyhow::anyhow!(error_message))
     }
 }
 
