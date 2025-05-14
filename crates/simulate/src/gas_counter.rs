68e62f0b573543360ce7b1ed1614adba168181db
marijamijailovic <mijailovicmarija@hotmail.com>
2025-05-14 14:11:38 +0200

    Add initial gas for calls from version constants

---
+++ crates/simulate/src/gas_counter.rs
@@ -0,0 +1,34 @@
+use blockifier::execution::call_info::CallInfo;
+use starknet_api::execution_resources::GasAmount;
+
+#[derive(Debug)]
+pub(crate) struct GasCounter {
+    pub(crate) spent_gas: GasAmount,
+    pub(crate) remaining_gas: GasAmount,
+}
+
+impl GasCounter {
+    pub(crate) fn new(initial_gas: GasAmount) -> Self {
+        GasCounter {
+            spent_gas: GasAmount(0),
+            remaining_gas: initial_gas,
+        }
+    }
+
+    fn spend(&mut self, amount: GasAmount) {
+        self.spent_gas = self.spent_gas.checked_add(amount).expect("Gas overflow");
+        self.remaining_gas = self
+            .remaining_gas
+            .checked_sub(amount)
+            .expect("Overuse of gas; should have been caught earlier");
+    }
+
+    /// Limits the amount of gas that can be used (in validate\execute) by the given global limit.
+    pub(crate) fn limit_usage(&self, amount: GasAmount) -> u64 {
+        self.remaining_gas.min(amount).0
+    }
+
+    pub(crate) fn subtract_used_gas(&mut self, call_info: &CallInfo) {
+        self.spend(GasAmount(call_info.execution.gas_consumed));
+    }
+}
