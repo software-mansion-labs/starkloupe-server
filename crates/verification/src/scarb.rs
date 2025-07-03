bff564af5ff776546f76dc9227bf599233ff436a
Marija Mijailovic <mijailovicmarija@hotmail.com>
2025-07-03 13:48:54 +0200

    New message in case SIGKILL is sent

--- crates/verification/src/scarb.rs
+++ crates/verification/src/scarb.rs
@@ -92,8 +92,8 @@ fn run_project_build_for_profile(tmp_dir: &PathBuf, path: &str, profile: &str) -
 
     if !output.status.success() {
         let stdout = String::from_utf8_lossy(&output.stdout);
-
-        return Err(anyhow::anyhow!("{}\n{}", output.status, stdout));
+        error!("Build failed. Status: {:?}, Stdout: {:?}", output.status, stdout);
+        return Err(anyhow::anyhow!("The verification process was terminated due to resource limits. Please contact support for assistance."));
     }
 
     Ok(())
